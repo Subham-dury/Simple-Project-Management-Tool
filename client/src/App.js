a1a0ee75b1329ec837563d1538532578bee39cb6
Subham Kar Chowdhury <karchowdhurysubham@gmail.com>
2023-10-05 00:32:40 +0530

    UseQuery to fetch clients

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
+import Client from "./components/Client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Client />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 require("dotenv").config();
-const {graphqlHTTP} = require('express-graphql')
-const schema = require('./schema/schema')
-const connectDB = require('./config/db')
-const colors = require('colors')
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
+app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
